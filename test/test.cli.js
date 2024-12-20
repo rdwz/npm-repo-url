44feb73ec3d18b220ac7327320f36e07e19475f0
Marc Redwerkz <redwerkz@gmail.com>
2024-12-20 23:05:45 +0100

    init: 🎉

--- test/test.cli.js
+++
@@ -1,8 +0,0 @@
-import cli from '../src/cli.js'
-
-describe('CLI', () => {
-  test('should print help message', async () => {
-    const output = await cli(['--help'])
-    expect(output).toContain('Usage:')
-  })
-})
